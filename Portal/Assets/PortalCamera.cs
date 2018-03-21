75d3f7497e312da518d52d41812b341ab966e9e1
flzzyf <2981019122@qq.com>
2018-03-21 17:58:38 +0800

    镜头贴图

--- Portal/Assets/PortalCamera.cs
+++ Portal/Assets/PortalCamera.cs
@@ -12,5 +12,12 @@ public class PortalCamera : MonoBehaviour {
     {
         Vector3 offset = playerCamera.position - otherPortal.position;
         transform.position = portal.position + offset;
+
+        float angularDifference = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+        Quaternion rotationDifference = Quaternion.AngleAxis(angularDifference, Vector3.up);
+        Vector3 newCameraDir = rotationDifference * playerCamera.forward;
+
+        transform.rotation = Quaternion.LookRotation(newCameraDir, Vector3.up);
 	}
 }
