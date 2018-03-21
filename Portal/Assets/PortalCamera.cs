da6475f278386b64accf9cca9b80a7c7c02efb87
flzzyf <2981019122@qq.com>
2018-03-21 17:35:52 +0800

    镜头贴图

---
+++ Portal/Assets/PortalCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour {
+
+    public Transform playerCamera;
+    public Transform portal;
+    public Transform otherPortal;
+
+	void Update () 
+    {
+        Vector3 offset = playerCamera.position - otherPortal.position;
+        transform.position = portal.position + offset;
+	}
+}
