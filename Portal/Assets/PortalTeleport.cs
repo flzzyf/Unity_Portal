443adf33df01ff48fb8a2d8dac74b74c56ce1dc8
flzzyf <2981019122@qq.com>
2018-03-24 18:16:14 +0800

    完成，但还有bug

---
+++ Portal/Assets/PortalTeleport.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleport : MonoBehaviour {
+
+    public Transform player;
+    public Transform receiver;
+
+    bool playerInThePortal = false;
+	
+	void Update () 
+    {
+        if(playerInThePortal)
+        {
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            if(dotProduct < 0)
+            {
+                float rotationDiff = Quaternion.Angle(transform.rotation, receiver.rotation);
+                rotationDiff += 180;
+                player.Rotate(Vector3.up, rotationDiff);
+
+                Vector3 posOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+                player.position = receiver.position + posOffset;
+
+                playerInThePortal = false;
+                print("juice");
+            }
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player"){
+            playerInThePortal = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerInThePortal = false;
+        }
+    }
+}
--- Portal/Assets/PortalTextureSetup.cs
+++ Portal/Assets/PortalTextureSetup.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PortalTextureSetup : MonoBehaviour {
 
+    public Camera cameraA;
+    public Material cameraMatA;
     public Camera cameraB;
     public Material cameraMatB;
 
@@ -12,6 +14,10 @@ public class PortalTextureSetup : MonoBehaviour {
         cameraB.targetTexture.height = Screen.height;
         cameraB.targetTexture.width = Screen.width;
         cameraB.targetTexture.depth = 24;
+
+        cameraA.targetTexture.height = Screen.height;
+        cameraA.targetTexture.width = Screen.width;
+        cameraA.targetTexture.depth = 24;
 
 	}
 
